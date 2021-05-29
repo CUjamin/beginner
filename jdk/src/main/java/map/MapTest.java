d74b3be1997ad051fe2156af82c484d796b30c52
cujamin <chbnjamin@gmail.com>
2021-05-29 15:46:44 +0800

    MapTest

---
+++ jdk/src/main/java/map/MapTest.java
@@ -0,0 +1,17 @@
+package map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MapTest {
+    private Map<String, String> testMap() {
+        HashMap<String, String> newMap = new HashMap<>();
+        ConcurrentHashMap<String, String> newConcurrentHashMap = new ConcurrentHashMap<>();
+        newMap.put("1", "1");
+        newConcurrentHashMap.put("1", "1");
+        String cvaule = newConcurrentHashMap.get("1");
+        String value = newMap.get("1");
+        return newMap;
+    }
+}
